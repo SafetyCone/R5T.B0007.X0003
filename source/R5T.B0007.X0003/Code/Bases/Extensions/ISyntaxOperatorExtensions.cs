76514a4e837bca6dad2ff8861008eb2e83b1de61
davidcoats <david@getsafetycone.com>
2022-06-09 12:17:08 -0700

    Interim changes.

---
+++ source/R5T.B0007.X0003/Code/Bases/Extensions/ISyntaxOperatorExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+
+using R5T.B0007;
+
+
+namespace System
+{
+    public static class ISyntaxOperatorExtensions
+    {
+        public static TNode Indent<TNode>(this ISpacingOperator _,
+            TNode node,
+            SyntaxTriviaList indentation)
+            where TNode : SyntaxNode
+        {
+            var output = node.Indent(indentation);
+            return output;
+        }
+
+        public static SyntaxToken Indent(this ISpacingOperator _,
+            SyntaxToken token,
+            SyntaxTriviaList indentation)
+        {
+            var output = token.Indent(indentation);
+            return output;
+        }
+    }
+}
--- source/R5T.B0007.X0003/Code/Interfaces/Interface1.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-
-namespace R5T.B0007.X0003
-{
-    public interface Interface1
-    {
-    }
-}
